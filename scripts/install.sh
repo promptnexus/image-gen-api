c18680e10083f1aeb3ce12e3beed32fd0c932916
Bilal Itani <bilalitani1@gmail.com>
2025-05-18 14:42:29 -0500

    Fix poetry bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -e  # fail on any error
+
 cd /home/ec2-user/imagegen
 
-export PATH="$HOME/.local/bin:$PATH"
-if ! command -v poetry &> /dev/null; then
-  curl -sSL https://install.python-poetry.org | python3 -
-fi
+# Install poetry to a known location under /opt
+curl -sSL https://install.python-poetry.org | python3 - --yes --install-dir /opt/poetry
 
-poetry install
+# Make poetry available in PATH for this script
+export PATH="/opt/poetry/bin:$PATH"
+
+# Install project dependencies
+/opt/poetry/bin/poetry install --no-interaction
